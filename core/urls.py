68b8fe7c32bffec9f93b6f3626ce0ca986589edf
safwanvk <safwanvalakundil@gmail.com>
2020-06-28 18:25:15 +0530

    Set LinkListView

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 
 from django.urls import path
 
+from . views import LinkListView
+
 urlpatterns = [
+    path('', LinkListView.as_view(), name='link-list')
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from .models import Link
+
+
+class LinkListView(ListView):
+    model = Link
+    template_name = "link_list.html"
