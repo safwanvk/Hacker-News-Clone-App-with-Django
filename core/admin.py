7ac3b177247555f1ee1674e74b23ea3430383827
safwanvk <safwanvalakundil@gmail.com>
2020-06-28 18:13:36 +0530

    Create models Link & Vote

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Link, Vote
+
+admin.site.register(Link)
+admin.site.register(Vote)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Link(models.Model):
+    title = models.CharField(max_length=250)
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+    submitted_on = models.DateTimeField(auto_now_add=True)
+    vote_score = models.FloatField(default=0.0)
+    url = models.URLField(max_length=250, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Vote(models.Model):
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
