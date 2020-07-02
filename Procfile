67d11b35882b453780a0c77f3c3a6c330d9fd974
safwanvk <safwanvalakundil@gmail.com>
2020-07-02 15:41:48 +0530

    upload to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hacker.wsgi
--- hacker/settings.py
+++ hacker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 LOGIN_REDIRECT_URL = 'core:link-list'
 LOGOUT_REDIRECT_URL = '/'
 
@@ -130,3 +135,6 @@ STATICFILES_DIRS = [
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
